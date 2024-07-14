3e8720afdf037c8bf4becc6e5a8035cb4c4ee724
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-07-14 22:47:45 +0600

    feat: add copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
   entry: path.resolve(__dirname, './src/index'),
@@ -53,6 +54,9 @@ const baseConfig = {
       filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: ['_redirects'],
+    }),
   ],
 };
 
